8e6f57e8cbc46fa486a9cd716d1dfd9feb87e805
Rahul Kumar Singh <148378696+Rahul-Singh18@users.noreply.github.com>
2026-03-14 00:32:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,4 +1,5 @@
-SELECT score,
-       DENSE_RANK() OVER (ORDER BY score DESC) AS "rank" 
-FROM Scores;
-
+SELECT 
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
+FROM Scores
+ORDER BY score DESC;
