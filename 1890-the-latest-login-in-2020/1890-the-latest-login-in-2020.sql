5fc51c4096f5c15c7dedae10635a46a3f3e9987f
Rahul Kumar Singh <148378696+Rahul-Singh18@users.noreply.github.com>
2026-04-17 00:16:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,11 +1,5 @@
 # Write your MySQL query statement below
--- select user_id, max(time_stamp) as last_stamp
--- from logins
--- where time_stamp >= '2020-01-01' and time_stamp <= '2020-12-31'
--- group by user_id
-SELECT 
-    user_id,
-    MAX(time_stamp) AS last_stamp
+SELECT user_id, MAX(time_stamp) AS last_stamp
 FROM Logins
 WHERE YEAR(time_stamp) = 2020
 GROUP BY user_id;
