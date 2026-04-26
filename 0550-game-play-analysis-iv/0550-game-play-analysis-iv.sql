12d18debf1a167f5a53bfd944f21c054d9ae88de
Rahul Kumar Singh <148378696+Rahul-Singh18@users.noreply.github.com>
2026-04-26 23:40:24 +0530

    Time: 584 ms (34.08%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,21 +1,5 @@
-WITH first_login AS (
-    SELECT player_id, MIN(event_date) AS first_date
-    FROM Activity
-    GROUP BY player_id
-)
-SELECT 
-    ROUND(
-        SUM(
-            CASE 
-              WHEN EXISTS (
-                  SELECT 1 
-                  FROM Activity a 
-                  WHERE a.player_id = f.player_id 
-                    AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY)
-              ) 
-              THEN 1 ELSE 0 
-            END
-        ) * 1.0 / COUNT(*), 
-        2
-    ) AS fraction
-FROM first_login f;
+select round(sum(if(player_login=1,1,0))/count(distinct player_id),2) as fraction
+from
+(select player_id,
+datediff(event_date,min(event_date) over(partition by player_id))=1 as player_login
+from Activity) as new_activity
