2f493edf92366ffc87747d02664135e4d76ec04c
Rahul Kumar Singh <148378696+Rahul-Singh18@users.noreply.github.com>
2026-04-28 23:51:13 +0530

    Time: 332 ms (21.59%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT id, COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id, accepter_id AS friend
+    FROM RequestAccepted
+    UNION
+    SELECT accepter_id AS id, requester_id AS friend
+    FROM RequestAccepted
+) AS all_friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
