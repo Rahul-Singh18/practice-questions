b5cf3a2e969083bdb4b2604e63225347995c2327
Rahul Kumar Singh <148378696+Rahul-Singh18@users.noreply.github.com>
2026-04-28 23:53:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select id, count(id) as num from
+select id, count(id) as num 
+from
 (select requester_id as id from RequestAccepted 
 union all
 select accepter_id as id from RequestAccepted) temp
